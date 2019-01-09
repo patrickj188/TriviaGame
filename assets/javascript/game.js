b405177417fe3deaf710c4474f804100e64f5436
patrickj188@gmail.com <Patrickj188@gmail.com>
2019-01-09 17:56:56 -0600

    fixed reset bug

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
-var panel =$('#quiz-area');
+var quizArea =$('#quiz-area');
 var countStartNumber = 30;
 
 
 
-$(document).on('clcik', '#start-over', function(e){
+$(document).on('click', '#start-over', function(e){
     game.reset(e);
 });
 
@@ -15,7 +15,6 @@ $(document).on('click', '#start', function(e) {
     $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter-number">30</span> Seconds</h2>');
     game.loadQuestion(e);
   });
-
 
 
 const questions = [{
@@ -64,9 +63,9 @@ var game = {
     },
     loadQuestion: function(){
       timer = setInterval(game.countdown, 1000);
-      panel.html('<h2>' + questions[this.currentQuestion].question + '</h2>' );
+      quizArea.html('<h2>' + questions[this.currentQuestion].question + '</h2>' );
       for (var i = 0; i<questions[this.currentQuestion].answers.length; i++){
-        panel.append('<button class="answer-button" id="button"' + 'data-name="' + questions[this.currentQuestion].answers[i] + '">' + questions[this.currentQuestion].answers[i]+ '</button>');
+        quizArea.append('<button class="answer-button" id="button"' + 'data-name="' + questions[this.currentQuestion].answers[i] + '">' + questions[this.currentQuestion].answers[i]+ '</button>');
       }
     },
     nextQuestion: function(){
@@ -79,9 +78,9 @@ var game = {
       clearInterval(timer);
       $('#counter-number').html(game.counter);
   
-      panel.html('<h2>Out of Time!</h2>');
-      panel.append('<h3>The Correct Answer was: ' + questions[this.currentQuestion].correctAnswer);
-      panel.append('<img src="' + questions[this.currentQuestion].image + '" />');
+      quizArea.html('<h2>Out of Time!</h2>');
+      quizArea.append('<h3>The Correct Answer was: ' + questions[this.currentQuestion].correctAnswer);
+      quizArea.append('<img src="' + questions[this.currentQuestion].image + '" />');
   
       if (game.currentQuestion === questions.length - 1){
         setTimeout(game.results, 3 * 1000);
@@ -92,12 +91,12 @@ var game = {
     results: function() {
       clearInterval(timer);
   
-      panel.html('<h2>All done, heres how you did!</h2>');
+      quizArea.html('<h2>All done, heres how you did!</h2>');
       $('#counter-number').html(game.counter);
-      panel.append('<h3>Correct Answers: ' + game.correct + '</h3>');
-      panel.append('<h3>Incorrect Answers: ' + game.incorrect + '</h3>');
-      panel.append('<h3>Unanswered: ' + (questions.length - (game.incorrect + game.correct)) + '</h3>');
-      panel.append('<br><button id="start-over">Start Over?</button>');
+      quizArea.append('<h3>Correct Answers: ' + game.correct + '</h3>');
+      quizArea.append('<h3>Incorrect Answers: ' + game.incorrect + '</h3>');
+      quizArea.append('<h3>Unanswered: ' + (questions.length - (game.incorrect + game.correct)) + '</h3>');
+      quizArea.append('<br><button id="start-over">Start Over?</button>');
     },
     clicked: function(e) {
       clearInterval(timer);
@@ -111,9 +110,9 @@ var game = {
     answeredIncorrectly: function() {
       game.incorrect++;
       clearInterval(timer);
-      panel.html('<h2>Nope!</h2>');
-      panel.append('<h3>The Correct Answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
-      panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
+      quizArea.html('<h2>Nope!</h2>');
+      quizArea.append('<h3>The Correct Answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+      quizArea.append('<img src="' + questions[game.currentQuestion].image + '" />');
   
       if (game.currentQuestion === questions.length - 1){
         setTimeout(game.results, 3 * 1000);
@@ -124,8 +123,8 @@ var game = {
     answeredCorrectly: function(){
       clearInterval(timer);
       game.correct++;
-      panel.html('<h2>Correct!</h2>');
-      panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
+      quizArea.html('<h2>Correct!</h2>');
+      quizArea.append('<img src="' + questions[game.currentQuestion].image + '" />');
   
       if (game.currentQuestion === questions.length - 1){
         setTimeout(game.results, 3 * 1000);
@@ -139,7 +138,7 @@ var game = {
       this.correct = 0;
       this.incorrect = 0;
       this.loadQuestion();
-      panel.append('#start-over');
     }
+
   
   };
